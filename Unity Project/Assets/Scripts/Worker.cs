40ef5a1420687640a40ebb0a264a7a25a6caa6b6
Arco Mul <me@arcomul.nl>
2014-03-02 21:58:42 +0100

    Give idle workers a funny movement so that the scene looks alive

---
+++ Unity Project/Assets/Scripts/Worker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Worker : MonoBehaviour
+{
+	public enum States {Idle = 1}
+	public States State = States.Idle;
+
+	private Vector3 StartPosition;
+
+	void Start ()
+	{
+		StartPosition = transform.position;
+	}
+
+	void Update ()
+	{
+		if (State == States.Idle)
+		{
+			transform.position = StartPosition - new Vector3(2.5f * Time.deltaTime, 2.5f * Time.deltaTime, 0) + new Vector3(Random.value * 5f * Time.deltaTime, Random.value * 5f * Time.deltaTime, 0);
+		}
+	}
+}
