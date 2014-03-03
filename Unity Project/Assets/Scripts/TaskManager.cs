c46be2c56f786414ef904f6158880f9ed2c06da3
YannickLange <yannick_lange_94@hotmail.com>
2014-03-03 15:31:27 +0100

    Added first version of taskmanager

---
+++ Unity Project/Assets/Scripts/TaskManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TaskManager : MonoBehaviour {
+	public GameObject Building;
+	public List<TaskBuilding> Tasks;
+
+	// Use this for initialization
+	void Start () {
+		Tasks = new List<TaskBuilding> ();
+		CreateBuilding ();
+	}
+
+	public void CreateBuilding()
+	{
+		GameObject Obj = (GameObject) Instantiate (Building, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), new Quaternion(0, 0, 0, 0)); 
+		Tasks.Add(Obj.GetComponent<TaskBuilding>());
+	}
+
+	public void FinishCurrentPaintTask()
+	{
+
+	}
+	
+	public void FinishCurrentRepairTask()
+	{
+		
+	}
+
+}
