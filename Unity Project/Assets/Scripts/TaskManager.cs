ba39baedec2790641c7948521f77195f073d1d68
YannickLange <yannick_lange_94@hotmail.com>
2014-03-03 15:54:19 +0100

    Taskmanager in progress

--- Unity Project/Assets/Scripts/TaskManager.cs
+++ Unity Project/Assets/Scripts/TaskManager.cs
@@ -6,15 +6,23 @@ public class TaskManager : MonoBehaviour {
 	public GameObject Building;
 	public List<TaskBuilding> Tasks;
 
+	private int BuildingCount = 0;
+
 	// Use this for initialization
 	void Start () {
 		Tasks = new List<TaskBuilding> ();
 		CreateBuilding ();
+		CreateBuilding ();
+		CreateBuilding ();
 	}
-
+	 
 	public void CreateBuilding()
 	{
-		GameObject Obj = (GameObject) Instantiate (Building, new Vector3(transform.position.x - (Tasks.Count), transform.position.y, transform.position.z), new Quaternion(0, 0, 0, 0)); 
-		Tasks.Add(Obj.GetComponent<TaskBuilding>());
+		BuildingCount++;
+		GameObject Obj = (GameObject) Instantiate (Building, new Vector3(transform.position.x + (Tasks.Count * 2), transform.position.y, transform.position.z), new Quaternion(0, 0, 0, 0)); 
+		TaskBuilding TaskBuilding = Obj.GetComponent<TaskBuilding> ();
+		TaskBuilding.TimeToFailTask = 120 - (BuildingCount * 10);
+		TaskBuilding.TimeToFinishTask = 30 + (BuildingCount * 10);
+		Tasks.Add(TaskBuilding);
 	}	
 }
