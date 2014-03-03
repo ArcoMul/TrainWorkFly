8ed988cac2a173d11557b28c4c51319c721d5f22
Arco Mul <me@arcomul.nl>
2014-03-03 14:34:30 +0100

    Show score in screen + add point when finishing task

---
+++ Unity Project/Assets/Scripts/Score.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+	private static Score _Instance = null;
+	public static Score Instance
+	{
+		get
+		{
+			if (_Instance == null)
+				_Instance = FindObjectOfType<Score>();
+			return _Instance;
+		}
+	}
+
+	public TextMesh Text;
+
+	private int _Points = 0;
+	public int Points {
+		get {
+			return _Points;
+		}
+		set {
+			_Points = value;
+			Text.text = "Score: " + _Points;
+		}
+	}
+}
