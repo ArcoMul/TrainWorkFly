88b626dbbd357502f6cd37bbb2b607be469b3f7f
YannickLange <yannick_lange_94@hotmail.com>
2014-03-11 16:00:28 +0100

    Removed moving tasks

--- Unity Project/Assets/Scripts/TaskBuilding.cs
+++ Unity Project/Assets/Scripts/TaskBuilding.cs
@@ -35,23 +35,6 @@ public class TaskBuilding : Building
 
 	void Update()
 	{
-		if (State == States.Moving)
-		{
-			Debug.Log("Moving");
-			// Get the direction from the worker to the building and reset the z axis
-			Vector3 Direction = MoveToPosition - transform.position;
-			Direction = new Vector3(Direction.x, Direction.y, 0);
-			
-			// Calculate the movement for this frame and add this to the position
-			Vector3 Movement = Direction.normalized * Time.deltaTime * 2;
-			transform.position += Movement;
-			
-			// If the movement is bigger than the actual length to walk in total, switch to idle
-			//  (-0.01f is a small margin to make sure we always switch)
-			if (Mathf.Abs(Movement.x) > Mathf.Abs(Direction.x) - 0.01f && Mathf.Abs(Movement.y) > Mathf.Abs(Direction.y) - 0.01f) {
-				State = States.Idle;
-			}
-		}
 		double TimeSpanSinceSpawn = DateTime.Now.Subtract(SpawnTime).TotalSeconds;
 		FailBar.SetPercentage((float) TimeSpanSinceSpawn / TimeToFailTask );
 
