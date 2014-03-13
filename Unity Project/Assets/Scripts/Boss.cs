8b757467fa3dfd4ee69824896a199ac0d5fb97a7
marijnz <marijn@marijnzwemmer.com>
2014-03-13 10:17:45 +0100

    Boss movement changed to wasd controls

--- Unity Project/Assets/Scripts/Boss.cs
+++ Unity Project/Assets/Scripts/Boss.cs
@@ -21,21 +21,12 @@ public class Boss : MonoBehaviour
     {
 
 		if (!Walking) return;
-	    // move
-	    // Get the direction from the worker to the building and reset the z axis
-	    Vector3 Direction = MoveToPosition - transform.position;
-	    Direction = new Vector3(Direction.x, Direction.y, 0);
 
-	    // Calculate the movement for this frame and add this to the position
-	    Vector3 Movement = Direction.normalized * Time.deltaTime * 2;
-	    transform.position += Movement;
+        transform.position += transform.right * Input.GetAxis("Horizontal") * 3 * Time.deltaTime;
+        transform.position += transform.up * Input.GetAxis("Vertical") * 3 * Time.deltaTime;
 
-	    // If the movement is bigger than the actual length to walk in total, switch to idle
-	    //  (-0.01f is a small margin to make sure we always switch)
-	    if (Mathf.Abs(Movement.x) > Mathf.Abs(Direction.x) - 0.01f && Mathf.Abs(Movement.y) > Mathf.Abs(Direction.y) - 0.01f)
-	    {
-	        Walking = false;
-	    }
+
+
     }
 
 	public void WalkToCurrentMousePosition ()
